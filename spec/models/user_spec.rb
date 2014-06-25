1e32c3a9e6d7db2ac86faa01c818edad40c134c0
Clément Uster <clement.uster@gmail.com>
2014-06-25 15:08:16 +0200

    make a user model with password

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,39 @@
+require 'spec_helper'
+
+describe User do
+  it "needs a password and confirmation to save" do
+    u = User.new(name: "steve")
+    u.save
+
+    expect(u).to_not be_valid
+
+    u.password = "password"
+    u.password_confirmation = ""
+    u.save
+
+    expect(u).to_not be_valid
+
+    u.password_confirmation = "password"
+    u.save
+
+    expect(u).to be_valid
+  end
+
+  it "needs password confirmation to match" do
+    u = User.create(name: "steve", password: "hunter", password_confirmation: "hunter2")
+
+    expect(u).to_not be_valid
+  end
+end
+
+describe "authentication" do
+  let(:user){ User.create(name: "steve", password: "hunter2", password_confirmation: "hunter2") }
+  
+  it "authenticate with a correct password" do
+    expect(user.authenticate("hunter2")).to be
+  end
+
+  it "does not authenticate with an incorrect password" do
+    expect(user.authenticate("hunter1")).to_not be
+  end
+end
